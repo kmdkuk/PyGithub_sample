e42284936d784a3f4f4a1e45bd32c61fd6608cb9
kouki <kouworld0123@gmail.com>
2018-01-18 17:11:25 +0900

    検索結果をずらっと表示するsample完成

--- sample.py
+++ sample.py
@@ -1,4 +1,12 @@
 from github import Github
-g = Github("user", )
-for repo in g.get_user().get_repos():
-        print repo.name
+import setting
+g = Github(setting.Github_User, setting.Github_Pass)
+'''for repo in g.get_user().get_repos():
+        print(repo.name)'''
+sample_list = g.search_code("sample+extension:py")
+
+for result in sample_list:
+    print(result)
+
+print(sample_list)
+
